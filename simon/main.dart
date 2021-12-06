0d61f4051cfef378bbdc5e274aef4713571e0b70
S-ecki <75510543+S-ecki@users.noreply.github.com>
2021-12-06 17:02:51 +0100

    skeleton day 7

--- simon/main.dart
+++ simon/main.dart
@@ -9,6 +9,7 @@ final days = <GenericDay>[
   Day04(),
   Day05(),
   Day06(),
+  Day07(),
 ];
 
 void main() {
---
+++ simon/solutions/day07.dart
@@ -0,0 +1,18 @@
+import '../utils/index.dart';
+
+class Day07 extends GenericDay {
+  Day07() : super(7);
+
+  @override
+  parseInput() {}
+
+  @override
+  int solvePart1() {
+    return 0;
+  }
+
+  @override
+  int solvePart2() {
+    return 0;
+  }
+}
