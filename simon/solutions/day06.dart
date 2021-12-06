045c09b2392c35b86d53a252215ec44264a9fd71
S-ecki <75510543+S-ecki@users.noreply.github.com>
2021-12-06 11:30:10 +0100

    solution day 6

--- simon/solutions/day06.dart
+++ simon/solutions/day06.dart
@@ -1,18 +1,36 @@
-import '../utils/generic_day.dart';
+import '../utils/index.dart';
 
 class Day06 extends GenericDay {
   Day06() : super(6);
 
   @override
-  parseInput() {}
+  List<int> parseInput() {
+    // list of fish
+    final fish = ParseUtil.stringListToIntList(input.getBy(','));
+    // list with number of fish for each age, where age == index
+    final ages = List.filled(9, 0);
+    fish.forEach((f) => ++ages[f]);
+
+    return ages;
+  }
 
   @override
   int solvePart1() {
-    return 0;
+    return solve(80);
   }
 
   @override
   int solvePart2() {
-    return 0;
+    return solve(256);
+  }
+
+  int solve(int days) {
+    final ages = parseInput();
+
+    for (var i = 0; i < days; ++i) {
+      ages[(7 + i) % 9] += ages[i % 9];
+    }
+
+    return ages.reduce((sum, age) => sum + age);
   }
 }
--- simon/utils/parse_util.dart
+++ simon/utils/parse_util.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import '../solutions/day04.dart';
 
 class ParseUtil {
