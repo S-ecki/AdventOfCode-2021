5e2c43584496b53dfa1666b8562ff9279839d575
S-ecki <75510543+S-ecki@users.noreply.github.com>
2021-12-07 11:31:52 +0100

    solution day 7

--- solutions/day07.dart
+++ solutions/day07.dart
@@ -1,18 +1,50 @@
+import 'dart:math';
+
 import '../utils/index.dart';
 
 class Day07 extends GenericDay {
   Day07() : super(7);
 
   @override
-  parseInput() {}
+  List<int> parseInput() {
+    return ParseUtil.stringListToIntList(input.getBy(','));
+  }
 
   @override
   int solvePart1() {
-    return 0;
+    final positions = parseInput();
+    final median = _median(positions);
+    return positions.fold<int>(0, (s, pos) => s + (pos - median).abs());
   }
 
   @override
   int solvePart2() {
-    return 0;
+    final positions = parseInput();
+    final mean = _mean(positions), mCeil = mean.ceil(), mFloor = mean.floor();
+
+    final ceil = positions.fold<int>(0, (s, pos) => s + _gaus((pos - mCeil)));
+    final floor = positions.fold<int>(0, (s, pos) => s + _gaus((pos - mFloor)));
+
+    return min(ceil, floor);
+  }
+
+  int _median(List<int> list) {
+    list.sort((a, b) => a.compareTo(b));
+
+    int middle = list.length ~/ 2;
+    return list.length % 2 == 1
+        ? list[middle]
+        : ((list[middle - 1] + list[middle]) / 2.0).round();
+  }
+
+  double _mean(List<int> list) {
+    return (list.reduce((a, b) => a + b) / list.length);
+  }
+
+  /// gauß = n(n+1)/2 \
+  /// round gets called to cast it to int, the result is an interger by definition
+  /// either way
+  int _gaus(int i) {
+    return ((i.abs() * (i.abs() + 1)) / 2).round();
   }
 }
