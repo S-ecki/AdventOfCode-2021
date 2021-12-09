2efe9400a09a12bc77903e344e1d5370a4abd08d
S-ecki <75510543+S-ecki@users.noreply.github.com>
2021-12-09 02:03:58 +0100

    Boilerplate autogeneration script

---
+++ solutions/day09.dart
@@ -0,0 +1,23 @@
+import '../utils/index.dart';
+
+class Day09 extends GenericDay {
+  Day09() : super(9);
+
+  @override
+  parseInput() {
+    
+  }
+
+  @override
+  int solvePart1() {
+    
+    return 0;
+  }
+
+  @override
+  int solvePart2() {
+
+    return 0;
+  }
+}
+
