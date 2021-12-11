edb33b40099eea538b88d4221ee68b26e7dca753
S-ecki <75510543+S-ecki@users.noreply.github.com>
2021-12-11 19:54:52 +0100

    refactor day 11

--- solutions/day11.dart
+++ solutions/day11.dart
@@ -1,7 +1,11 @@
+import 'package:tuple/tuple.dart';
+
 import '../utils/index.dart';
 
 class Day11 extends GenericDay {
   Day11() : super(11);
+
+  late Tuple2<int, int> solution;
 
   @override
   Board parseInput() {
@@ -13,59 +17,93 @@ class Day11 extends GenericDay {
 
   @override
   int solvePart1() {
-    final board = parseInput();
-    final height = board.length;
-    final width = board[0].length;
-    int flashes = 0;
+    solution = solve();
+    return solution.item1;
+  }
 
-    //gameloop
-    for (var i = 0; i < 100; ++i) {
-      final flashPoints = <Position>{};
-      final didFlash = <Position>{};
-      // loop board
+  @override
+  int solvePart2() {
+    return solution.item2;
+  }
+
+  Tuple2<int, int> solve() {
+    final board = parseInput();
+    final boardSize = Tuple2(board.length, board[0].length);
+    // convenience method to loop over 2D array (board)
+    final forBoard = (VoidBoardCallback f) {
       for (var y = 0; y < board.length; ++y) {
         for (var x = 0; x < board[y].length; ++x) {
-          // increment first
-          ++board[y][x];
-          // check for flashes
-          if (board[y][x] >= 10) {
-            flashPoints.add(Position(x, y));
-          }
+          f(x, y);
         }
       }
-      // now flash
-      while (flashPoints.isNotEmpty) {
-        // take first flashpoint and get neighboars to falsh
-        final pos = flashPoints.first;
-        flashPoints.remove(pos);
-        didFlash.add(pos);
+    };
 
-        final flashNeigh = _neighboards(pos.item1, pos.item2, height, width);
-        flashNeigh.removeWhere((p) => didFlash.contains(p));
+    late Tuple2<int, int> solution = Tuple2(0, 0);
 
-        flashNeigh.forEach((p) {
+    // hold the results for the parts respectively
+    int flashCount = 0;
+    int roundCount = 0;
+
+    // loop breaks as soon as all octupuses flash at the same time, giving
+    // us the result to part 2 (the round where this happens)
+    while (true) {
+      // for part 1, we count the flashes for the first 100 rounds
+      if (roundCount++ == 100) {
+        solution = solution.withItem1(flashCount);
+      }
+
+      final pointsToFlash = <Position>{};
+      // is needed so points do not flash multiple times per round
+      final pointsDidFlash = <Position>{};
+
+      forBoard((x, y) {
+        // increment each position first and check if any flash
+        ++board[y][x];
+        if (board[y][x] >= 10) pointsToFlash.add(Position(x, y));
+      });
+
+      // now flash - loop as new points that flash may arise
+      while (pointsToFlash.isNotEmpty) {
+        // take any flashpoint and look for neighboars to flash
+        final flashPoint = pointsToFlash.first;
+        pointsToFlash.remove(flashPoint);
+        pointsDidFlash.add(flashPoint);
+
+        // take all neighbours that will get flashed, but remove already
+        // flashed to avoid infinite loops
+        final neighboursToFlash = _neighboards(flashPoint.item1,
+            flashPoint.item2, boardSize.item1, boardSize.item2)
+          ..removeAll(pointsDidFlash);
+
+        neighboursToFlash.forEach((p) {
           ++board[p.item2][p.item1];
-          if (board[p.item2][p.item1] >= 10) {
-            flashPoints.add(Position(p.item1, p.item2));
-          }
+          if (board[p.item2][p.item1] >= 10)
+            pointsToFlash.add(Position(p.item1, p.item2));
         });
       }
 
-      for (var y = 0; y < board.length; ++y) {
-        for (var x = 0; x < board[y].length; ++x) {
-          if (board[y][x] >= 10) {
-            ++flashes;
-            board[y][x] = 0;
-          }
+      final flashesBeforeRound = flashCount;
+      // loop over board and flash all with value >= 10
+      forBoard((x, y) {
+        if (board[y][x] >= 10) {
+          ++flashCount;
+          board[y][x] = 0;
         }
+      });
+
+      // if the flashcount increased this round by the board area, it means
+      // that every point flashed. the round this happens in is our part 2 solution
+      if (flashCount - flashesBeforeRound ==
+          boardSize.item1 * boardSize.item2) {
+        solution = solution.withItem2(roundCount);
+        break;
       }
     }
-
-    return flashes;
+    return solution;
   }
 
-  List<Position> _neighboards(int x, int y, int height, int width) {
-    final positions = <Position>[];
+  Set<Position> _neighboards(int x, int y, int height, int width) {
+    final positions = <Position>{};
     for (var xx = x - 1; xx <= x + 1; ++xx) {
       for (var yy = y - 1; yy <= y + 1; ++yy) {
         if (xx >= 0 &&
@@ -78,61 +116,5 @@ class Day11 extends GenericDay {
       }
     }
     return positions;
-  }
-
-  @override
-  int solvePart2() {
-    final board = parseInput();
-    final height = board.length;
-    final width = board[0].length;
-
-    //gameloop
-    int step = 0;
-    while (true) {
-      ++step;
-      final flashPoints = <Position>{};
-      final didFlash = <Position>{};
-      // loop board
-      for (var y = 0; y < board.length; ++y) {
-        for (var x = 0; x < board[y].length; ++x) {
-          // increment first
-          ++board[y][x];
-          // check for flashes
-          if (board[y][x] >= 10) {
-            flashPoints.add(Position(x, y));
-          }
-        }
-      }
-      // now flash
-      while (flashPoints.isNotEmpty) {
-        // take first flashpoint and get neighboars to falsh
-        final pos = flashPoints.first;
-        flashPoints.remove(pos);
-        didFlash.add(pos);
-
-        final flashNeigh = _neighboards(pos.item1, pos.item2, height, width);
-        flashNeigh.removeWhere((p) => didFlash.contains(p));
-
-        flashNeigh.forEach((p) {
-          ++board[p.item2][p.item1];
-          if (board[p.item2][p.item1] >= 10) {
-            flashPoints.add(Position(p.item1, p.item2));
-          }
-        });
-      }
-
-      int flashcount = 0;
-      for (var y = 0; y < board.length; ++y) {
-        for (var x = 0; x < board[y].length; ++x) {
-          if (board[y][x] >= 10) {
-            ++flashcount;
-            board[y][x] = 0;
-          }
-        }
-      }
-      if (flashcount == height * width) {
-        return step;
-      }
-    }
   }
 }
--- utils/types.dart
+++ utils/types.dart
@@ -5,3 +5,4 @@ typedef Position = Tuple2<int, int>;
 typedef Basin = Set<Position>;
 typedef StringList = List<String>;
 typedef Command = Tuple2<StringList, StringList>;
+typedef VoidBoardCallback = void Function(int, int);
